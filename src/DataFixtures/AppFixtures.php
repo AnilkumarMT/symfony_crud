c7ccd44f3cdb01bc7a3593e18a392cac5604ab19
anil_msi <mtanimt@gmail.com>
2026-02-19 23:33:30 +0530

    Initial commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Product;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $product = new Product();
+        $product->setName('Product One');
+        $product->setDescription('Product one description');
+        $product->setSize('100');
+        $manager->persist($product);
+
+        $product = new Product();
+        $product->setName('Product Two');
+        $product->setDescription('Product Two description');
+        $product->setSize('200');
+        $manager->persist($product);
+
+        $manager->flush();
+    }
+}
